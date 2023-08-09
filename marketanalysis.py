b3a4a8bbf23be1bbd83dc425a4e1e6ccf2e76bf3
Bijay Shrestha <bshrestha-1@ou.edu>
2023-08-09 14:00:00 -0500

    added kmeans

---
+++ marketanalysis.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+from sklearn.preprocessing import StandardScaler
+
+# Load the data
+file_path = 'boardgames.csv'
+df = pd.read_csv(file_path)
+
+# Assume boardgamedesigner and boardgamemechanic are cleaned and ready to use
+df['transactions'] = df.apply(
+    lambda x: [x['boardgamedesigner']] + str(x['boardgamemechanic']).split(', '), axis=1
+)
+
+# Splitting the dataset into training and testing sets
+train_df, test_df = train_test_split(df, test_size=0.2, random_state=42)
+
+# Convert transactions to the format needed for MBA
+def get_transaction_data(data_frame):
+    transactions = data_frame['transactions'].tolist()
+    te = TransactionEncoder()
+    te_ary = te.fit(transactions).transform(transactions)
+    return pd.DataFrame(te_ary, columns=te.columns_)
+
+train_transactions = get_transaction_data(train_df)
+test_transactions = get_transaction_data(test_df)
+
+# Generating rules from the training set
+frequent_itemsets_train = apriori(train_transactions, min_support=0.01, use_colnames=True)
+rules_train = association_rules(frequent_itemsets_train, metric="confidence", min_threshold=0.5)
+
+# Evaluating the rules on the test set
+# First, we need to adjust how we evaluate the presence of antecedents:
+def match_antecedents(row, rules):
+    for antecedents in rules['antecedents']:
+        if row[list(antecedents)].all():
+            return True
+    return False
+
+test_transactions['matches'] = test_transactions.apply(match_antecedents, axis=1, rules=rules_train)
+test_support = test_transactions['matches'].mean()
+
+print("Test Support:", test_support)
+print(rules_train[['antecedents', 'consequents', 'support', 'confidence', 'lift']])
+
+rules_train.to_csv('association_rules.csv', index=False)
+
